8719b04fb0c0509e97a4537913ba7de310a3462c
Jag <jagdeepsingh3939@gmail.com>
2019-06-19 17:16:04 -0700

    added webpack files to eslint ignore

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+      name: '',
+    };
+  }
+
   render() {
     return (
       <>
